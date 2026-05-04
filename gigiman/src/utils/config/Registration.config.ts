1d3c2be6624861146fb56873eab4dbbe35e0cd33
Suryaprakash2310 <suryaprakash33207@gmail.com>
2026-05-04 16:02:05 +0530

    fix bug

--- gigiman/src/utils/config/Registration.config.ts
+++ gigiman/src/utils/config/Registration.config.ts
@@ -1,8 +1,8 @@
 import { UserRole } from "../enums/CommonEnum";
 
 export const REGISTRATION_CATEGORIES = [
-  { id: UserRole.SINGLE_EMPLOYEE, title: 'Single employee',  icon: require('../../../assets/test.png') },
-  { id: UserRole.MULTI_EMPLOYEE, title: 'Multi employee',  icon: require('../../../assets/icons/construction.png') },
-  { id: UserRole.TOOL_SHOP, title: 'Tool shop',  icon: require('../../../assets/icons/home_cleaning.png') },
+  { id: UserRole.SINGLE_EMPLOYEE, title: 'Single employee', icon: require('../../../assets/icons/singleEmp.png') },
+  { id: UserRole.MULTI_EMPLOYEE, title: 'Multi employee', icon: require('../../../assets/icons/multiEmp.png') },
+  { id: UserRole.TOOL_SHOP, title: 'Tool shop', icon: require('../../../assets/icons/toolshop.png') },
   // Add more categories as needed
 ];
